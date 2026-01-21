b5ca403b83bbd7d7f9d23526017044c7d273eece
somiseol <somi.seol@proton.me>
2026-01-21 14:33:01 -0600

    added duplication error msg and test case in author.py

--- lab2/author.py
+++ lab2/author.py
@@ -4,7 +4,11 @@ class Author:
         self.books = [] 
 
     def publish(self, title):
-        self.books.append(title)
+        if title in self.books:
+            print(f"duplicate error: {title}")
+            # raise error?
+        else:
+            self.books.append(title)
 
     def __str__(self):
         return f"{self.name} {self.books}"
@@ -20,5 +24,12 @@ def main():
     # test case w/o books
     bierce = Author("Ambrose Bierce")
     print(bierce)
+
+    # test case w duplicates
+    zweig = Author("Stefan Zweig")
+    zweig.publish("Beware of Pity")
+    zweig.publish("Fear")
+    zweig.publish("Fear") # duplicate
+    print(zweig)
 
 main()
