c703f8e47802d14c28b1ccec2af329262a91d5db
somiseol <somi.seol@proton.me>
2026-01-25 12:56:59 -0600

    created branch-merge dir for hw

---
+++ branch-merge/camelCase.py
@@ -0,0 +1,27 @@
+import re
+
+phrase = input("phrase: ")
+
+# check if str is valid
+if not re.fullmatch("^[a-zA-Z ]+$", phrase):
+    print("invalid")
+    quit()
+
+phrase = phrase.lower()
+new_phrase = ""
+
+i = 0
+
+# python reassigns i when using for loop !!!
+while i < len(phrase):
+    if phrase[i] == " ":
+        new_phrase += phrase[i + 1].upper()
+        i += 2
+    else:
+        new_phrase += phrase[i]
+        i += 1
+
+# use .capitalize() next time
+# error if trailing white space
+
+print(new_phrase)
