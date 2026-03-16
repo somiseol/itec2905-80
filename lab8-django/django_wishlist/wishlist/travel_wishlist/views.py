11fb22d4dd93dbb90960f57488c3a61b486169c4
somiseol <somi.seol@proton.me>
2026-03-16 13:05:38 -0500

    ordered place list in /

--- lab8-django/django_wishlist/wishlist/travel_wishlist/views.py
+++ lab8-django/django_wishlist/wishlist/travel_wishlist/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Place
 
 def place_list(request):
-    places = Place.objects.filter(visited=False)
+    places = Place.objects.filter(visited=False).order_by('name')
     return render(request, 'travel_wishlist/wishlist.html', {'places': places})
