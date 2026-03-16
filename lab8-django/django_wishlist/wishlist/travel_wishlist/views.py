24acdf6a521c954d8cfa87543c2da6abbb07943a
somiseol <somi.seol@proton.me>
2026-03-16 13:03:23 -0500

    render list of places; filtered visited only

--- lab8-django/django_wishlist/wishlist/travel_wishlist/views.py
+++ lab8-django/django_wishlist/wishlist/travel_wishlist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Place
 
 def place_list(request):
-    return render(request, 'travel_wishlist/wishlist.html')
+    places = Place.objects.filter(visited=False)
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places})
